1ac36d70617567d4622ff4004698bbdda759671c
Allan Wilson <allan@allanwilson.net>
2016-10-24 21:38:35 +0300

    improved file_searcher

--- alluCloud2/moduleFilmSearch/film_search.py
+++ alluCloud2/moduleFilmSearch/film_search.py
@@ -2,9 +2,19 @@
 
 import json
 import urllib2
+from urllib2 import URLError
+
+'''
+Errors to catch...
+1) No results:                        JSON format error, None
+2) Bad URL or no network:             urllib2.URLError: <urlopen error [Errno 11001] getaddrinfo failed>
+'''
 
 def search_words_return_titles(search_term):
-    url = "http://www.omdbapi.com/?s={}&y=&plot=short&r=json".format(search_term)
+    try:
+        url = "http://www.omdbapi.co/?s={}&y=&plot=short&r=json".format(search_term)
+    except URLError, e:
+        print e.reason
     response = json.load(urllib2.urlopen(url))
     return response
 
